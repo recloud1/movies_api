0057b42cb06e5b88f89058265819e1154b884f32
Andrey Guliaiko <guliaiko@llcglobaladvisors.com>
2022-10-03 16:08:24 +0400

    add genres service change GenreBare to GenreBase

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -1,21 +1,42 @@
-from fastapi import APIRouter, Depends
+from typing import Optional
 
-from models.genres import GenreList
+from fastapi import APIRouter, Depends, Query, Path
+
+from models.core import GetMultiQueryParam
+from models.genres import GenreBase, GenreList
+from models.params import SearchValue, Search
 from services.genres import get_genre_service, GenreElasticService
 
 genres = APIRouter()
 
 
 @genres.get(
-    '/',
+    '',
     response_model=GenreList,
     summary='Получение списка жанров',
+    description='Список жанров с возможность поиска и сортировки',
 )
 async def get_genres(
-        genre_service: GenreElasticService = Depends(get_genre_service)):
-    pass
+        genre_service: GenreElasticService = Depends(get_genre_service),
+        query_params: GetMultiQueryParam = Depends(),
+        search: Optional[str] = Query(None, description='Поиск по жанрам'),
+) -> GenreList:
+    results, count = await genre_service.get_multi(
+        query_params=query_params,
+        search=Search(values=[SearchValue(field='name', value=search)]) if search else None
+    )
+    return GenreList(**query_params.dict(), rows_number=count, data=results)
 
 
-@genres.get('/{genre_id}')
-async def get_genre():
-    pass
+@genres.get(
+    '/{genre_id}',
+    response_model=GenreBase,
+    summary='Получение информации о конкретном жанре',
+    description='Получить жанр по его ID',
+)
+async def get_genre(
+    genre_id: str = Path(...),
+    genre_service: GenreElasticService = Depends(get_genre_service),
+) -> Optional[GenreBase]:
+    result = await genre_service.get(_id=genre_id, model=GenreBase)
+    return result
